6dd05a1f3485d61bce4e5d75b1c4eebb36274e2b
Kouki Saito <dan.addr.skd@gmail.com>
2019-06-17 09:12:00 +0900

    第1節までのコード追加
    - プロジェクト作成
    - CocoaPodsのインストール
    - SwiftLintのインストール
    - Fastlaneのインストール

---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '11.0'
+
+target 'TodoLog' do
+  use_frameworks!
+
+  pod 'SwiftLint' 
+
+  target 'TodoLogTests' do
+    inherit! :search_paths
+  end
+
+  target 'TodoLogUITests' do
+    inherit! :search_paths
+  end
+
+end
+
---
+++ TodoLog/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TodoLog
+//
+//  Created by Kouki Saito on 2019/06/17.
+//  Copyright © 2019 Kouki Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
