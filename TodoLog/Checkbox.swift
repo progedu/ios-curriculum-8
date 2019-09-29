44eff1c69566c79c4170c6032b4306b126ba293e
Kouki Saito <dan.addr.skd@gmail.com>
2019-09-29 23:06:56 +0900

    第２節までのコード追加
    - UITableViewControllerの追加
    - セルのレイアウト作成
    - IBDesignableとIBInspectableを使ってCheckBox.swiftを実装

---
+++ TodoLog/Checkbox.swift
@@ -0,0 +1,27 @@
+//
+//  Checkbox.swift
+//  TodoLog
+//
+//  Created by Kouki Saito on 9/29/19.
+//  Copyright © 2019 Kouki Saito. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class CheckBox: UIControl {
+
+    @IBInspectable
+    var checked: Bool = false
+
+    override func draw(_ rect: CGRect) {
+        let bundle = Bundle(for: CheckBox.self)
+        let image: UIImage
+        if checked {
+            image = UIImage(named: "check_true", in: bundle, compatibleWith: nil)!
+        } else {
+            image = UIImage(named: "check_false", in: bundle, compatibleWith: nil)!
+        }
+        image.draw(in: bounds)
+    }
+}
