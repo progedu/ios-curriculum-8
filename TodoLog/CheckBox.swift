547be5bb89b72e1902684e37441395e2805844cc
Kouki Saito <dan.addr.skd@gmail.com>
2019-12-01 22:34:20 +0900

    第2節までのコード追加
    - TableViewでTODOリストを追加
    - IBDesignableを使ったチェックボックスを追加

---
+++ TodoLog/CheckBox.swift
@@ -0,0 +1,26 @@
+//
+//  CheckBox.swift
+//  TodoLog
+//
+//  Created by Kouki Saito on 5/27/19.
+//  Copyright © 2019 Kouki Saito. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class CheckBox: UIControl {
+
+    @IBInspectable var checked: Bool = false
+
+    override func draw(_ rect: CGRect) {
+        let bundle = Bundle(for: CheckBox.self)
+        let image: UIImage
+        if checked {
+            image = UIImage(named: "check_true", in: bundle, compatibleWith: nil)!
+        } else {
+            image = UIImage(named: "check_false", in: bundle, compatibleWith: nil)!
+        }
+        image.draw(in: bounds)
+    }
+}
