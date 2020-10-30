f699747006f498601cd85bb521f91b66cb370243
Kouki Saito <dan.addr.skd@gmail.com>
2020-10-30 18:46:49 +0900

    WIP

--- TodoLog/CheckBox.swift
+++ TodoLog/CheckBox.swift
@@ -11,21 +11,26 @@ import UIKit
 @IBDesignable
 final class CheckBox: UIControl {
 
-    @IBInspectable var checked: Bool = false
+    @IBInspectable var checked: Bool = false {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
 
     override func draw(_ rect: CGRect) {
         let bundle = Bundle(for: CheckBox.self)
         let image: UIImage
         if checked {
             image = UIImage(named: "check_true", in: bundle, compatibleWith: nil)!
+            accessibilityLabel = "Checked"
         } else {
             image = UIImage(named: "check_false", in: bundle, compatibleWith: nil)!
+            accessibilityLabel = "Unchecked"
         }
         image.draw(in: bounds)
     }
 
     func toggle() {
         self.checked = !checked
-        setNeedsDisplay()
     }
 }
--- TodoLogUITests/TodoLogUITests.swift
+++ TodoLogUITests/TodoLogUITests.swift
@@ -26,9 +26,78 @@ class TodoLogUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_新しいTodoを追加して完了できる() {
+        let app = XCUIApplication()
+        // キャンセルできるかを確認
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.element.buttons["キャンセル"].tap()
+
+        // Todoを追加する
+        let random = Int.random(in: 0..<10000)
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.element.textFields["Title"].tap()
+        app.alerts.element.textFields["Title"].typeText("タイトル\(random)")
+        app.alerts.element.textFields["LocationInfo"].tap()
+        app.alerts.element.textFields["LocationInfo"].typeText("場所")
+        app.alerts.element.buttons["登録"].tap()
+        XCTAssertEqual(app.cells.element(boundBy: 0).staticTexts["Title"].label, "タイトル\(random)")
+
+        // Todoを完了する
+        XCTAssertEqual(
+            app.cells.element(boundBy: 0).otherElements["CheckBox"].label,
+            "Unchecked"
+        )
+        app.cells.element(boundBy: 0).otherElements["CheckBox"].tap()
+        XCTAssertEqual(
+            app.cells.element(boundBy: 0).otherElements["CheckBox"].label,
+            "Checked"
+        )
     }
 
+    func test_メッセージでTodoのログを記録できる() {
+        let app = XCUIApplication()
+
+        // メッセージ追加のテストに使うTodoを追加する
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.element.textFields["Title"].tap()
+        app.alerts.element.textFields["Title"].typeText("メッセージテスト")
+        app.alerts.element.textFields["LocationInfo"].tap()
+        app.alerts.element.textFields["LocationInfo"].typeText("テスト")
+        app.alerts.element.buttons["登録"].tap()
+        app.cells.element(boundBy: 0).tap()
+
+        // メッセージを送信する
+        app.textViews["ChatInput"].tap()
+        app.textViews["ChatInput"].typeText("これは1つ目のテストメッセージです")
+        app.buttons["SendButton"].tap()
+
+        // 送信後にテキストフィールドが空になっているか確認する
+        XCTAssertEqual(app.textViews["ChatInput"].label, "")
+
+        app.textViews["ChatInput"].tap()
+        app.textViews["ChatInput"].typeText("これは2つ目のテストメッセージです")
+        app.buttons["SendButton"].tap()
+
+        XCTAssertEqual(
+            app.collectionViews.cells.element(boundBy: 0).staticTexts.firstMatch.label,
+            "これは1つ目のテストメッセージです"
+        )
+        XCTAssertEqual(
+            app.collectionViews.cells.element(boundBy: 1).staticTexts.firstMatch.label,
+            "これは2つ目のテストメッセージです"
+        )
+
+        // 画面を開き直してもデータが消えないことを確認する
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        app.cells.element(boundBy: 0).tap()
+        XCTAssertEqual(
+            app.collectionViews.cells.element(boundBy: 0).staticTexts.firstMatch.label,
+            "これは1つ目のテストメッセージです"
+        )
+        XCTAssertEqual(
+            app.collectionViews.cells.element(boundBy: 1).staticTexts.firstMatch.label,
+            "これは2つ目のテストメッセージです"
+        )
+
+    }
 }
